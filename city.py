59b4c8ff7bd52901eb2408ff43f8d48d58432ffe
Hossein <fazel.hosein9654@gmail.com>
2024-03-18 13:04:46 +0330

    add city class and read from file function

--- city.py
+++ city.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from collections import defaultdict as dd
+from typing import Any
 
-@dataclass
+@dataclass()
 class edge:
-    value : float = float("inf")
     line : str
     vehicle : str
+    value : float = float("inf")
 
 class paths:
     def __init__(self):
@@ -27,3 +29,45 @@ class paths:
         for item in self.edges:
             if item.line == line and item.vehicle == name:
                 return item
+
+
+class Tehran:
+    def __init__(self):
+        self.city_graph = dd(lambda : dd(paths))
+    
+    def read_from_file(self, *names):
+        for name in names:
+            try:
+                file = open(f"{name}.txt", 'r')
+                line = file.readline().replace(":\n" , "")
+                stat1 = ""
+                stat2 = ""
+                dist = 0
+                while True:
+                    # read parts like line, station1, station2 and distance
+                    stat1 = file.readline().replace("\n", "")
+                    if stat1 == "" or line == "":
+                        break
+                    elif stat1 == "end" :
+                        line = file.readline()
+                        continue
+                    stat2 = file.readline().replace("\n", "")
+                    dist = int(file.readline().replace("\n", ""))
+
+                    if line[0] == 'l':
+                        e1 = edge(dist, line, "Subway")
+                        e2 = edge(dist, line, "Taxi")
+
+                        self.city_graph[stat1][stat2].add_edge(e1)
+                        self.city_graph[stat1][stat2].add_edge(e2)
+
+                        self.city_graph[stat2][stat1].add_edge(e1)
+                        self.city_graph[stat2][stat1].add_edge(e2)
+
+                    elif line[0] == 'b':
+                        e1 = edge(dist, line, "Bus")
+
+                        self.city_graph[stat1][stat2].add_edge(e1)
+                        self.city_graph[stat2][stat1].add_edge(e1)
+            except:
+                raise ValueError(f"There is no file with name {name}")
---
+++ seman_cmd.py
@@ -0,0 +1,13 @@
+from city import Tehran
+from Time import Time
+
+src = input()
+dest = input()
+t1 = Time(input())
+
+t1 = Tehran()
+try:
+    t1.read_from_file("line", "Bus")
+
+except:
+    print("oops some file does not exist")
